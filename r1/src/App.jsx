3d932aac9003e29e9c4eaef3c41af958d77c8a9d
Ausra <munduswebstudio@gmail.com>
2023-05-09 13:29:13 +0300

    React start

---
+++ r1/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Racoon from './Components/004/Racoon';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <Racoon />
+
+      </header>
+    </div>
+  );
+
+}
+
+export default App;
---
+++ r1/src/Components/004/Racoon.jsx
@@ -0,0 +1,11 @@
+function Racoon() {
+
+    return (
+        <>
+            <span className="blue">small</span>
+            <h2 className="red">R-A-C-<i className="blue">00</i>-N</h2>
+        </>
+        );
+
+}
+export default Racoon;
