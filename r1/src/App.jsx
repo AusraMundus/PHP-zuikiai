8cf11fcd69249ff5b10c809a8141770317fc9dfa
Ausra <munduswebstudio@gmail.com>
2023-05-09 14:30:44 +0300

    React

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -3,11 +3,22 @@ import Racoon from './Components/004/Racoon';
 
 function App() {
 
+  const racoons = [
+    {size: 20, niceColor: 'crimson', name: 'Simas'},
+    {size: 50, niceColor: 'orange', name: 'Rimas'},
+    {size: 10, niceColor: 'pink', name: 'Sigutė'},
+    {size: 30, niceColor: 'gray', name: 'Antanas'},
+    {size: 30, niceColor: 'lightsalmon', name: 'Ona'},
+    {size: 60, niceColor: 'lime', name: 'Jadvyga'}
+]
+
   return (
     <div className="App">
       <header className="App-header">
         
-        <Racoon />
+      {
+        racoons.sort((a, b) => b.size - a.size).map(r => <Racoon key={r.name} size={r.size} niceColor={r.niceColor} name={r.name}/>)
+      }
 
       </header>
     </div>
--- r1/src/Components/004/Racoon.jsx
+++ r1/src/Components/004/Racoon.jsx
@@ -1,9 +1,26 @@
-function Racoon() {
+import rand from "../../Function/rand";
+import randColor from "../../Function/randColor";
+
+function Racoon({size, niceColor, name}) {
 
     return (
         <>
-            <span className="blue">small</span>
-            <h2 className="red">R-A-C-<i className="blue">00</i>-N</h2>
+            {
+                rand(0, 1)
+                    ? <span className="blue">{name}</span>
+                    : <span className="red">{name}</span>
+            }
+
+            <h2 className="red" style={{
+                fontSize: size + 'px',
+                color: niceColor
+            }}
+            
+            >
+                Rac
+                <i className="blue">00</i>
+                n
+            </h2>
         </>
         );
 
